0d53c88e2735af7f1154d122cfb7cf6beaeb74a7
Oopspw <290256660@qq.com>
2018-12-04 17:06:25 +0800

    change style and error

--- src/components/Offers/articleList.vue
+++ src/components/Offers/articleList.vue
@@ -1,26 +1,42 @@
 <template>
   <div v-title="$route.meta.title">
     <v-head></v-head>
+    <!-- 文章列表 -->
+    <div class="articleList">
+      <ul class="articleUl">
+        <!-- 单图文章 -->
+        <li class="articleLi">
+          <div class="articleInfo">
+            <div class="imgBox">
+              <img src>
+            </div>
+            <div class="contentBox">
+              <h3>ACTICLE TITLE</h3>
+            </div>
+          </div>
+        </li>
+        <!-- 三图文章 -->
+      </ul>
+    </div>
     <v-footer></v-footer>
   </div>
 </template>
 
 <script>
-
-import head from '../Modules/head'
-import footer from '../Modules/footer'
+import head from "../Modules/head";
+import footer from "../Modules/footer";
 export default {
-  name: 'articleList',
+  name: "articleList",
   data() {
     return {
-      url: ''
-    }
+      url: ""
+    };
   },
   components: {
     vHead: head,
     vFooter: footer
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
